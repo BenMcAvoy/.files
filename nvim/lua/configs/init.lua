3ba9c08fc0d48cc844a4ccec7fefe6b3824954ea
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-03 01:26:25 +0100

    Set some sensible defaults for vim

--- nvim/lua/configs/init.lua
+++ nvim/lua/configs/init.lua
@@ -1,2 +1,23 @@
 require("configs.bindings")
 require("configs.plugins")
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
