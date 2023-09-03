86c74a4332586904f69d8559376b0fcd807fa290
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-03 01:47:12 +0100

    Add gitsigns

--- nvim/lua/configs/plugins.lua
+++ nvim/lua/configs/plugins.lua
@@ -74,6 +74,8 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
+    use "lewis6991/gitsigns.nvim"
+
     if packer_bootstrap then
         require('packer').sync()
     end
