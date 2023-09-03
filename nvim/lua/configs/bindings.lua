55672ee6bc19eecb4e529c64f24ce284fc4967a9
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-03 01:26:30 +0100

    Clear highlighting on escape

--- nvim/lua/configs/bindings.lua
+++ nvim/lua/configs/bindings.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 
 -- Set a binding to go back to NetRW
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Clear search highlight on escape
+vim.keymap.set("n", "<Esc>", vim.cmd.nohl)
