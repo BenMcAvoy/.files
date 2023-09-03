a0fb50101bab7749cbc01caae1247f98eae4a7c2
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-03 01:27:25 +0100

    Reformat with new tabstop

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.on_attach(function(_, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 -- Configure lua language server for neovim
@@ -16,31 +16,31 @@ local cmp_action = lsp.cmp_action()
 local cmp = require('cmp')
 
 cmp.setup({
-	mapping = {
-		['<Tab>'] = cmp_action.luasnip_supertab(),
-		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-		['<CR>'] = cmp.mapping.confirm({select = false}),
-	},
+    mapping = {
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+    },
 
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
 
-	preselect = 'item',
+    preselect = 'item',
 
-	completion = {
-		completeopt = 'menu,menuone,noinsert'
-	},
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
 })
 
 local lspconfig = require('lspconfig')
 
 local servers = {
-	'lua_ls',
-	'rust_analyzer',
+    'lua_ls',
+    'rust_analyzer',
 }
 
 for _, server in ipairs(servers) do
-	lspconfig[server].setup({})
+    lspconfig[server].setup({})
 end
