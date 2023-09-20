70f199a5842b7ae5bbb44928d469b378bb8ddea8
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-20 20:23:40 +0100

    Improve appearance of autocompletion menu

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -13,10 +13,31 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
-  }
+  },
+
+  formatting = {
+    fields = {'abbr', 'kind', 'menu'},
+    format = require('lspkind').cmp_format({
+      -- mode = 'symbol',
+      maxwidth = 50,
+      ellipsis_char = '...',
+    })
+  },
+
+  mapping = {
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  },
+
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
 })
