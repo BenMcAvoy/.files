705dc92dee4b133b28f4cdb58d5bd96386a4499f
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-02-19 00:17:50 +0000

    Update scripts

--- setup.sh
+++ setup.sh
@@ -1,52 +1,55 @@
 #!/usr/bin/env bash
 
-function backup_file() {
-    file=$1
-    backup_file="${file}.bak"
+echo "Needs re-working!"
+exit -1
 
-    if [[ -f $file ]]; then
-        if [[ -f $backup_file ]]; then
-            i=1
-            while [[ -f "${file}.${i}.bak" ]]; do
-                i=$((i+1))
-            done
-            backup_file="${file}.${i}.bak"
-        fi
-        mv "$file" "$backup_file"
-        echo "Backup created: $backup_file"
-    else
-        echo "$file doesn't exist"
-    fi
-}
+# function backup_file() {
+#     file=$1
+#     backup_file="${file}.bak"
 
-function backup() {
-    path=$1
-    backup_path="${path}.bak"
+#     if [[ -f $file ]]; then
+#         if [[ -f $backup_file ]]; then
+#             i=1
+#             while [[ -f "${file}.${i}.bak" ]]; do
+#                 i=$((i+1))
+#             done
+#             backup_file="${file}.${i}.bak"
+#         fi
+#         mv "$file" "$backup_file"
+#         echo "Backup created: $backup_file"
+#     else
+#         echo "$file doesn't exist"
+#     fi
+# }
 
-    if [[ -d $path ]]; then
-        if [[ -d $backup_path ]]; then
-            i=1
-            while [[ -d "${path}.${i}.bak" ]]; do
-                i=$((i+1))
-            done
-            backup_path="${path}.${i}.bak"
-        fi
-        cp -r "$path" "$backup_path"
-        echo "Backup created: $backup_path"
-    elif [[ -f $path ]]; then
-        backup_file "$(dirname $path)/$(basename $path)"
-    else
-        echo "Path does not exist: $path"
-    fi
-}
+# function backup() {
+#     path=$1
+#     backup_path="${path}.bak"
 
-function link() {
-    echo "$(pwd)/$1"
-    echo "$HOME/$2"
+#     if [[ -d $path ]]; then
+#         if [[ -d $backup_path ]]; then
+#             i=1
+#             while [[ -d "${path}.${i}.bak" ]]; do
+#                 i=$((i+1))
+#             done
+#             backup_path="${path}.${i}.bak"
+#         fi
+#         cp -r "$path" "$backup_path"
+#         echo "Backup created: $backup_path"
+#     elif [[ -f $path ]]; then
+#         backup_file "$(dirname $path)/$(basename $path)"
+#     else
+#         echo "Path does not exist: $path"
+#     fi
+# }
 
-    ln -s "$(pwd)/$1" "$HOME/$2"
-}
+# function link() {
+#     echo "$(pwd)/$1"
+#     echo "$HOME/$2"
 
-backup "$HOME/.config/nvim"
+#     ln -s "$(pwd)/$1" "$HOME/$2"
+# }
 
-link "nvim" ".config/"
+# backup "$HOME/.config/nvim"
+
+# link "nvim" ".config/"
