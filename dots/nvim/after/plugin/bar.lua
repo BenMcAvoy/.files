497cf4bb146edb4049c684cd4488f29e64f7fd88
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-02-23 10:33:50 +0000

    Switch to custom bar

--- dots/nvim/after/plugin/bar.lua
+++ dots/nvim/after/plugin/bar.lua
@@ -1,36 +1,21 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
 require('lualine').setup {
   options = {
     theme = "gruvbox",
     component_separators = ':',
     section_separators = { left = '', right = '' },
   },
+
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', separator = { left = '|' }, right_padding = 2 },
     },
 
     lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
+    lualine_c = {},
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      { 'location', separator = { right = '|' }, left_padding = 2 },
     },
   },
-
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-
-  tabline = {},
-  extensions = {},
 }
