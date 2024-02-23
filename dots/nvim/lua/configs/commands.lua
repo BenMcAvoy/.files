443b6476e6763e2b203b6cc3a9480a41595180e1
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-02-23 11:25:01 +0000

    Rewrite CPFT in Lua and make it toggle rather than set

--- dots/nvim/lua/configs/commands.lua
+++ dots/nvim/lua/configs/commands.lua
@@ -1,3 +1,9 @@
-vim.cmd([[
-  command! -nargs=? CPFT let g:copilot_filetypes = { (empty(<q-args>) ? &filetype : <q-args>) : v:true }
-]])
+vim.api.nvim_create_user_command('CPT', function(args)
+  local filetype = args[1] or vim.bo.filetype
+  local enabled = vim.g.copilot_filetypes[filetype]
+  vim.g.copilot_filetypes = {[filetype] = not enabled}
+
+  local message = enabled and 'Disabling' or 'Enabling'
+  print(message .. ' Copilot for ' .. filetype)
+end, {nargs = '?', complete = 'filetype', desc = 'Toggle Copilot for a specific filetype'})
+
