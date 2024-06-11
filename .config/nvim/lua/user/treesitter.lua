ad59696cf6d0e17eb5b2cbd9d4ad5a49e8e1a35a
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-06-11 09:17:15 +0100

    feat(nvim): enable all treesitter features

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -1,3 +1,14 @@
 require("nvim-treesitter.configs").setup({
 	auto_install = true,
+	sync_install = false,
+
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+	},
 })
