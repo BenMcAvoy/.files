861cf927603adf0127cde2dc1dd0447c06e29a6c
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-06-12 14:04:56 +0100

    feat(dwm): upload dwm configs

---
+++ .config/dwm/bar.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+function append_to_output() {
+	current="$1"
+	to_append="$2"
+
+	# check if `to_append` does to contain the text `error`
+	if [[ $to_append == *"error"* ]]; then
+		echo "$current"
+	else
+		echo "$current | $to_append"
+	fi
+}
+
+function clock() {
+	echo -n "󰥔 $(date '+%Y-%m-%d %H:%M')"
+}
+
+function brightness() {
+	if [ ! -d /sys/class/backlight ]; then
+		return
+	fi
+
+	# get using brightnessctl (need to divide as 25% = 64)
+	brightness=$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')
+
+	# use truncation to cutoff deicmal instead of rounding
+	brightness=${brightness%.*}
+
+	echo -n " $brightness%"
+}
+
+function vpn() {
+	country=$(ipaddr | cut -d ' ' -f 4)
+
+	# check if openvpn is running
+	if pgrep -x "openvpn" > /dev/null; then
+		echo -n " $country"
+	else
+		echo -n "󰌿 $country"
+	fi
+}
+
+# Function to display the CPU load
+function cpu() {
+	cpu=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print  100 - $1"%"}')
+
+	echo -n " $cpu"
+}
+
+# Function to display the RAM usage
+function ram() {
+	free=$(free -h | grep Mem | awk '{print $3}' | tr -d "Gi")
+	total=$(free -h | grep Mem | awk '{print $2}' | tr -d "Gi")
+
+	echo -n " $free/$total GiB"
+}
+
+function battery() {
+	if [ ! -d /sys/class/power_supply/BAT0 ]; then
+		return # No battery found, don't display anything
+	fi
+
+	percentage=$(acpi | awk '{print $4}' | tr -d "%,")
+	icon=""
+
+	if [ "$percentage" -gt 75 ]; then
+		icon=""
+	elif [ "$percentage" -gt 50 ]; then
+		icon=""
+	elif [ "$percentage" -gt 25 ]; then
+		icon=""
+	else
+		icon=""
+	fi
+
+	echo -n "$icon  $percentage%"
+}
+
+while true; do
+	# Append the output of the functions to the `output` variable
+	output="  $(clock)"
+	output=$(append_to_output "$output" "$(brightness)")
+	output=$(append_to_output "$output" "$(vpn)")
+	output=$(append_to_output "$output" "$(cpu)")
+	output=$(append_to_output "$output" "$(ram)")
+	output=$(append_to_output "$output" "$(battery)")
+	xsetroot -name "$output  "
+	sleep 1
+done
